cd82cc887dc80bac2da2cb05bf0886d518426edb
volemy <minayobenevolence0@gmail.com>
2023-10-19 21:15:48 +0000

    pchr

---
+++ pchar_opcode.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * pchar - Print the character at the top of the stack.
+ * @stack: A pointer to the stack.
+ * @line_number: The line number of the opcode.
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	int value = (*stack)->n;
+
+	if (value < 0 || value > 127)
+
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	putchar((char)value);
+	putchar('\n');
+}
