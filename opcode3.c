12c0fed1375a7652db9767a94d56d3997de9150e
volemy <minayobenevolence0@gmail.com>
2023-10-19 21:21:38 +0000

    further commits

--- opcode3.c
+++ opcode3.c
@@ -37,33 +37,3 @@ void op_mod(stack_t **stack, unsigned int line_number)
 	*stack = tmp->next;
 	free(tmp);
 }
-
-
-/**
- * commenting - function that enables monty to comment
- * @stack: stack to sort through
- * @line_number: line number
- * @line: line
- */
-
-void commenting(char *line, unsigned int line_number, stack_t **stack)
-{
-	char *trimmed_line = trim(line);
-
-	if (strlen(trimmed_line) == 0)
-	{
-		return;
-	}
-
-	if (trimmed_line[0] == '#')
-	{
-		return;
-	}
-
-	char *opcode = strtok(trimmed_line, " \t\n\r");
-
-	if (opcode != NULL)
-	{
-		execute_opcode(opcode, stack, line_number);
-	}
-
