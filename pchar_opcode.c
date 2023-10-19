22f01194272eafa45bfcf2ef0bf58107ffcbeeca
volemy <minayobenevolence0@gmail.com>
2023-10-19 21:28:42 +0000

    pchr

--- pchar_opcode.c
+++ pchar_opcode.c
@@ -7,13 +7,15 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
+	int value;
+
 	if (!stack || !*stack)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	int value = (*stack)->n;
+	value = (*stack)->n;
 
 	if (value < 0 || value > 127)
 
